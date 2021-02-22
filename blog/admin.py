6a4105d61c4a9e777311dcb09d5e8b51ce7102c8
svyatkin-aleks <svyatkin.aleks@icloud.com>
2021-02-22 21:47:36 +0300

    End of first lesson

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Comment, Admin_Comment
 
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Admin_Comment)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    img = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Admin_Comment(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
